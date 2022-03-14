f60a34a64acfa9f3b2bd43d5faae7480fd8e6086
Naga Koushik <94678196+naga4121@users.noreply.github.com>
2022-03-14 06:55:22 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+ void input(float *base, float *height)
+{
+  printf("enter the values");
+  scanf("%f%f",base,height);
+}
+void find_area(float base , float height, float *area)
+{
+  *area=(base*height)/2;
+}
+void output(float base,float height,float area)
+{
+  printf("area of triangle with %f and %f is %f",base,height,base);
+}
+int main()
+{
+  float base,height,area;
+  input(&base,&height);
+  find_area(base,height,&area);
+  output(base,height,area);
+  return 0;
 }
